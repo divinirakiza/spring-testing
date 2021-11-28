a75a48a7d1d90f2e92dd56745a5fd26bd4f56e03
DivinIrakiza <irakizadivin@gmail.com>
2021-11-28 16:13:40 +0200

    (chore): Added APIResponse domain

--- src/main/java/com/practice/springtest/controllers/UserController.java
+++ src/main/java/com/practice/springtest/controllers/UserController.java
@@ -1,12 +1,14 @@
 package com.practice.springtest.controllers;
 
 import com.practice.springtest.models.User;
+import com.practice.springtest.models.dtos.UserDTO;
 import com.practice.springtest.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,6 +28,12 @@ public class UserController {
     @GetMapping("/users/{id}")
     public ResponseEntity<?> getUserById(@PathVariable("id") Integer id) {
         return ResponseEntity.status(HttpStatus.OK).body(this.userService.getById(id));
+    }
+
+    @GetMapping("/users")
+    public ResponseEntity<?> createUser(@RequestBody UserDTO userDTO) {
+        User user = new User(userDTO.getEmail(), userDTO.getFirstName(), userDTO.getFirstName(), userDTO.getType());
+        return ResponseEntity.status(HttpStatus.OK).body(this.userService.createUser(user));
     }
 
 
---
+++ src/main/java/com/practice/springtest/models/dtos/UserDTO.java
@@ -0,0 +1,27 @@
+package com.practice.springtest.models.dtos;
+
+public class UserDTO {
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String type;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
