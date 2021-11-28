db90d2f4a801b5e5f87d6499d8ceffc05b837fc1
DivinIrakiza <irakizadivin@gmail.com>
2021-11-28 15:55:08 +0200

    (chore): User Model

---
+++ src/main/java/com/practice/springtest/models/User.java
@@ -0,0 +1,74 @@
+package com.practice.springtest.models;
+
+
+@Entity
+@Table(name="users")
+public class User {
+    public Integer id;
+
+    private String email;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String type;
+
+    public User() {};
+
+    public User(Integer id, String email, String firstName, String lastName, String type) {
+        this.id = id;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.type = type;
+    }
+
+
+    public User(String email, String firstName, String lastName, String type) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.type = type;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
