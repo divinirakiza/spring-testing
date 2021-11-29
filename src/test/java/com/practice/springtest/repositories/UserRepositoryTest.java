e323df2809f5cb192738f382163e700a33dcdbb4
DivinIrakiza <irakizadivin@gmail.com>
2021-11-29 20:51:03 +0200

    (chore): UserRepositoryTests tests

--- src/test/java/com/practice/springtest/repositories/UserRepositoryTest.java
+++ src/test/java/com/practice/springtest/repositories/UserRepositoryTest.java
@@ -1,8 +1,10 @@
 package com.practice.springtest.repositories;
 
 import com.practice.springtest.models.User;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.List;
@@ -11,21 +13,25 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(SpringRunner.class)
+@DataJpaTest
 public class UserRepositoryTest {
 
     @Autowired
     private IUserRepository userRepository;
 
+    @Test
     public void getAll_Success() {
         List<User> users = this.userRepository.findAll();
         assertEquals(1, users.size());
     }
 
+    @Test
     public void getById_Success() {
         Optional<User> optionalUser = this.userRepository.findById(1);
         assertTrue(optionalUser.isPresent());
     }
 
+    @Test
     public void getById_NotFound() {
         Optional<User> optionalUser = this.userRepository.findById(4);
         assertFalse(optionalUser.isPresent());
